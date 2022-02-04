55d4654c543ecdd4d6e3aedb84bc164030668326
marrk85 <96385065+marrk85@users.noreply.github.com>
2022-02-04 21:18:25 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -35,7 +35,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("» ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ sᴏɴɢ ꜰʀᴏᴍ ꜰᴀʟʟᴇɴ sᴇʀᴠᴇʀ ʙᴀʙʏ​.")
+    m.edit("» ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ sᴏɴɢ ꜰʀᴏᴍ ᴍᴀʀʀᴋ sᴇʀᴠᴇʀ ʙᴀʙʏ​.")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
