c53f437307ae46168158cd1bb90a9b4d44e875d3
marrk85 <96385065+marrk85@users.noreply.github.com>
2022-05-08 07:59:35 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -12,7 +12,7 @@ async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/dfb645bbd6b7ea1540f92.jpg",
         caption=f"""✰ʜᴇʟʟᴏ... , 
-ᴍʏ ɴᴀᴍᴇ NIKKU MUSIC BOT.
+ᴍʏ ɴᴀᴍᴇ 𝙈𝘼𝙍𝙍𝙆 ✘ 𝙈𝙐𝙎𝙄𝘾.
 ᴀ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴜsɪᴄ sᴛʀᴇᴀᴍɪɴɢ ʙᴏᴛ
 ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ: /
 """,
